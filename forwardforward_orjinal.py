e6a1b024037408c8630291504f6fba4968efc18e
HALIL TASAK <57623316+htsk0180@users.noreply.github.com>
2023-04-14 14:33:31 +0300

    forward_forward_algoritmasi_deneyleri

--- forwardforward_orjinal.py
+++ forwardforward_orjinal.py
@@ -214,3 +214,219 @@ plt.plot(range(len(history.history["FinalLoss"])), history.history["FinalLoss"])
 plt.title("Loss over training")
 plt.show()
 
+
+
+
+########################
+# PYTORCH IMPLEMENTATION
+########################
+
+
+import torch
+import torch.nn as nn
+from tqdm import tqdm
+from torch.optim import Adam
+from torchvision.datasets import MNIST
+from torchvision.transforms import Compose, ToTensor, Normalize, Lambda
+from torch.utils.data import DataLoader
+
+def MNIST_loaders(train_batch_size=50000, test_batch_size=10000):
+
+    transform = Compose([
+        ToTensor(),
+        Normalize((0.1307,), (0.3081,)),
+        Lambda(lambda x: torch.flatten(x))])
+
+    train_loader = DataLoader(
+        MNIST('./data/', train=True,
+              download=True,
+              transform=transform),
+        batch_size=train_batch_size, shuffle=True)
+
+    test_loader = DataLoader(
+        MNIST('./data/', train=False,
+              download=True,
+              transform=transform),
+        batch_size=test_batch_size, shuffle=False)
+
+    return train_loader, test_loader
+
+def overlay_y_on_x(x, y):
+    x_ = x.clone()
+    x_[:, :10] *= 0.0
+    x_[range(x.shape[0]), y] = x.max()
+    return x_
+
+'''class Net(torch.nn.Module):
+
+    def __init__(self, dims):
+        super().__init__()
+        self.layers = []
+        for d in range(len(dims) - 1):
+            self.layers += [Layer(dims[d], dims[d + 1]).cuda()]
+
+    def predict(self, x):
+        goodness_per_label = []
+        for label in range(10):
+            h = overlay_y_on_x(x, label)
+            goodness = []
+            for layer in self.layers:
+                h = layer(h)
+                goodness += [(h.pow(2).sum(1) / h.shape[1]).pow(0.5)]
+            goodness_per_label += [sum(goodness).unsqueeze(1)]
+        goodness_per_label = torch.cat(goodness_per_label, 1)
+        return goodness_per_label.argmax(1)
+
+    def train(self, x_pos, x_neg):
+        h_pos, h_neg = x_pos, x_neg
+        for i, layer in enumerate(self.layers):
+            print('training layer', i, '...')
+            h_pos, h_neg = layer.train(h_pos, h_neg)'''
+import torch
+import math
+
+class Net(torch.nn.Module):
+    
+    def __init__(self, dims):
+        super().__init__()
+        self.layers = []
+        for d in range(len(dims) - 1):
+            self.layers += [Layer(dims[d], dims[d + 1]).cuda()]
+        
+    def predict(self, x):
+        goodness_per_label = []
+        for label in range(10):
+            h = overlay_y_on_x(x, label)
+            goodness = []
+            for layer in self.layers:
+                h = layer(h)
+                mean = h.mean(dim=1, keepdim=True)
+                std = h.std(dim=1, keepdim=True)
+                h_gauss = (h - mean) / (std + 1e-7)
+                goodness += [torch.exp(-0.5 * h_gauss.pow(2)) / (math.sqrt(2 * math.pi) * (std + 1e-7))]
+            goodness_per_label += [torch.cat(goodness, dim=1).sum(dim=1, keepdim=True)]
+        goodness_per_label = torch.cat(goodness_per_label, 1)
+        return goodness_per_label.argmax(1)
+    
+    def train(self, x_pos, x_neg):
+        h_pos, h_neg = x_pos, x_neg
+        for i, layer in enumerate(self.layers):
+            print('training layer', i, '...')
+            h_pos, h_neg = layer.train(h_pos, h_neg)
+
+import torch
+from torch.optim.optimizer import Optimizer
+
+
+class Lion(Optimizer):
+  r"""Implements Lion algorithm."""
+
+  def __init__(self, params, lr=1e-4, betas=(0.9, 0.99), weight_decay=0.0):
+    """Initialize the hyperparameters.
+
+    Args:
+      params (iterable): iterable of parameters to optimize or dicts defining
+        parameter groups
+      lr (float, optional): learning rate (default: 1e-4)
+      betas (Tuple[float, float], optional): coefficients used for computing
+        running averages of gradient and its square (default: (0.9, 0.99))
+      weight_decay (float, optional): weight decay coefficient (default: 0)
+    """
+
+    if not 0.0 <= lr:
+      raise ValueError('Invalid learning rate: {}'.format(lr))
+    if not 0.0 <= betas[0] < 1.0:
+      raise ValueError('Invalid beta parameter at index 0: {}'.format(betas[0]))
+    if not 0.0 <= betas[1] < 1.0:
+      raise ValueError('Invalid beta parameter at index 1: {}'.format(betas[1]))
+    defaults = dict(lr=lr, betas=betas, weight_decay=weight_decay)
+    super().__init__(params, defaults)
+
+  @torch.no_grad()
+  def step(self, closure=None):
+    """Performs a single optimization step.
+
+    Args:
+      closure (callable, optional): A closure that reevaluates the model
+        and returns the loss.
+
+    Returns:
+      the loss.
+    """
+    loss = None
+    if closure is not None:
+      with torch.enable_grad():
+        loss = closure()
+
+    for group in self.param_groups:
+      for p in group['params']:
+        if p.grad is None:
+          continue
+
+        # Perform stepweight decay
+        p.data.mul_(1 - group['lr'] * group['weight_decay'])
+
+        grad = p.grad
+        state = self.state[p]
+        # State initialization
+        if len(state) == 0:
+          # Exponential moving average of gradient values
+          state['exp_avg'] = torch.zeros_like(p)
+
+        exp_avg = state['exp_avg']
+        beta1, beta2 = group['betas']
+
+        # Weight update
+        update = exp_avg * beta1 + grad * (1 - beta1)
+        p.add_(torch.sign(update), alpha=-group['lr'])
+        # Decay the momentum running average coefficient
+        exp_avg.mul_(beta2).add_(grad, alpha=1 - beta2)
+
+    return loss
+
+class Layer(nn.Linear):
+    def __init__(self, in_features, out_features,
+                 bias=True, device=None, dtype=None):
+        super().__init__(in_features, out_features, bias, device, dtype)
+        self.relu = torch.nn.ReLU()
+        # self.opt = Lion(self.parameters(), lr=0.03)
+        self.opt = Adam(self.parameters(), lr=0.03)
+        self.threshold = 2.0
+        self.num_epochs = 100
+
+    def forward(self, x):
+        x_direction = x / (x.norm(2, 1, keepdim=True) + 1e-4)
+        return self.relu(
+            torch.mm(x_direction, self.weight.T) +
+            self.bias.unsqueeze(0))
+
+    def train(self, x_pos, x_neg):
+        for i in tqdm(range(self.num_epochs)):
+            g_pos = self.forward(x_pos).pow(4).mean(1)
+            g_neg = self.forward(x_neg).pow(4).mean(1)
+            loss = torch.log(1 + torch.exp(torch.cat([
+                -g_pos + self.threshold,
+                g_neg - self.threshold]))).mean()
+            self.opt.zero_grad()
+            loss.backward()
+            self.opt.step()
+        return self.forward(x_pos).detach(), self.forward(x_neg).detach()
+
+if __name__ == "__main__":
+    torch.manual_seed(1234)
+    train_loader, test_loader = MNIST_loaders()
+
+    net = Net([784, 500, 500])
+    x, y = next(iter(train_loader))
+    x, y = x.cuda(), y.cuda()
+    x_pos = overlay_y_on_x(x, y)
+    rnd = torch.randperm(x.size(0))
+    x_neg = overlay_y_on_x(x, y[rnd])
+    net.train(x_pos, x_neg)
+
+    print('train error:', 1.0 - net.predict(x).eq(y).float().mean().item())
+
+    x_te, y_te = next(iter(test_loader))
+    x_te, y_te = x_te.cuda(), y_te.cuda()
+
+    print('test error:', 1.0 - net.predict(x_te).eq(y_te).float().mean().item())
